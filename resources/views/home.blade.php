c5933861af54491927dfbffc725e13b9c7e388dd
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-07 16:41:52 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Home</h1>
+        
+        <ul>
+            <li><a href="{{route('about')}}">about</a></li>
+        </ul>
+    </body>
+</html>
